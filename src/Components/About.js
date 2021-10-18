bd5d9bf84b7accf616a26523ef3940d0f4aca68c
Adman94 <adman00794@gmail.com>
2021-10-18 14:11:26 -0700

    updated resume 10/18

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,7 +13,7 @@ class About extends Component {
          var zip = this.props.data.address.zip;
          var phone = this.props.data.phone;
          var email = this.props.data.email;
-         var resumeDownload = "https://drive.google.com/file/d/1qWOF5BGvsv03Cn2SoxDF1tiTElOaupXu/view?usp=sharing";
+         var resumeDownload = "https://drive.google.com/drive/u/0/folders/1NIrKXNROho1XS7iUVrJrforNJZ7DjGQJ";
       }
 
       return (
